1e47149330428a2883d116f5225572b566795cc0
Millie Chan <mireechan@gmail.com>
2016-08-11 01:03:01 -0700

    Require bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
   has_many :songs 
-
   validates :email, :password_hash, presence: true 
+  validates :email, uniqueness: true
 
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
+
+
+
