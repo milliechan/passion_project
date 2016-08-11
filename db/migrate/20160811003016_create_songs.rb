2d12f98d3c92b836ce73e9e83345a9b78fcd5c28
Millie Chan <mireechan@gmail.com>
2016-08-11 00:33:41 -0700

    Create migration for User and Song

--- db/migrate/20160811003016_create_songs.rb
+++ db/migrate/20160811003016_create_songs.rb
@@ -1,4 +1,10 @@
 class CreateSongs < ActiveRecord::Migration
   def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :url 
+      t.boolean :completed
+      t.references :user
+    end
   end
 end
