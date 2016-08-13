d1ff4c663f08fcf8eb578f1f9873bddf06e0f9fc
Millie Chan <mireechan@gmail.com>
2016-08-13 15:12:23 -0700

    Add bcrypt methods to user model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,10 @@
+#!/usr/bin/ruby
+# require 'rubygems'
+# gem 'google-api-client', '>0.7'
+# require 'google/api_client'
+# require 'trollop'
+
 class Song < ActiveRecord::Base
   belongs_to :user 
   validates :title, :url, presence: true 
-
-end
+end 
