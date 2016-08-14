6b5a754187665c1603ca2243a545c050a13343d9
Millie Chan <mireechan@gmail.com>
2016-08-13 17:27:27 -0700

    Add post method to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,17 @@ get '/' do
 end
 
 get '/users/new' do
-  erb :'users/index'
+  erb :'users/new'
+end
+
+# create a new user 
+post '/users/new' do 
+  @user = User.new(params[:user])
+
+  if @user.save 
+    session[:id] = @user.id 
+    redirect "/songs"
+  else 
+    erb :'users/new'
+  end
 end
