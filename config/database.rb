003fca59c6cc5ef8c5b184babe30556892262397
Millie Chan <mireechan@gmail.com>
2016-08-13 19:00:07 -0700

    WIP

--- config/database.rb
+++ config/database.rb
@@ -1,5 +1,3 @@
-require 'active_record'
-
 # Log queries to STDOUT in development
 if Sinatra::Application.development?
   ActiveRecord::Base.logger = Logger.new(STDOUT)
@@ -24,23 +22,21 @@ end
 
 # Heroku controls what database we connect to by setting the DATABASE_URL environment variable
 # We need to respect that if we want our Sinatra apps to run on Heroku without modification
-# db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{APP_NAME}_#{Sinatra::Application.environment}")
-# db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{APP_NAME}_#{Sinatra::Application.environment}")
 
-# DB_NAME = db.path[1..-1]
+DB_NAME = db.path[1..-1]
 
 # Note:
 #   Sinatra::Application.environment is set to the value of ENV['RACK_ENV']
 #   if ENV['RACK_ENV'] is set.  If ENV['RACK_ENV'] is not set, it defaults
 #   to :development
 
-# ActiveRecord::Base.establish_connection(
-#   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-#   :host     => db.host,
-#   :port     => db.port,
-#   :username => db.user,
-#   :password => db.password,
-#   :database => DB_NAME,
-#   :encoding => 'utf8'
-# )
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :port     => db.port,
+  :username => db.user,
+  :password => db.password,
+  :database => DB_NAME,
+  :encoding => 'utf8'
+)
