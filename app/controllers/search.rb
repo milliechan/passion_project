beda41da15544991f229310037c5d554c93e7d47
Millie Chan <mireechan@gmail.com>
2016-08-14 20:17:36 -0700

    Add song button creates a new song object in songs table

--- app/controllers/search.rb
+++ app/controllers/search.rb
@@ -3,9 +3,6 @@ get '/google817252d9e6d04b1e.html' do
 end
 
 get '/search' do 
-  p  "*" * 80 
-  p "search query: #{params[:query]}"
-
   query = params[:query]
   path = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&"
   path += "q=#{query}"
@@ -15,21 +12,10 @@ get '/search' do
   # GET https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=beyonce+karaoke&fields=items&key={YOUR_API_KEY}
 
   response = HTTParty.get(path)
-  # p response = response.to_json
   p  "*" * 80 
-  # p response["items"]
-  # response = response.to_json
-  # p response
-  # GET https://www.googleapis.com/youtube/v3/search?q=radiohead&key={YOUR_API_KEY}
-  # GET https://www.googleapis.com/youtube/v3/search
 
 
   @url = "https://www.youtube.com/watch?v="
-
-  # videoId = response.items[1].id.videoId 
-  # videoTitle = response.items[1].snippet.title
-  # videoPhoto = response.items[1].snippet.thumbnails.default.url
-  # thumbnail = items[1].snippet.thumbnails.default.url
   @songs = {} 
 
   response["items"].each_with_index do |item, index|
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,18 @@
 get '/songs' do 
-  
+  @queue = Song.all
   erb :'songs/index'
+end
+
+# post a song to the queue 
+post '/songs' do 
+  p "*" * 1000
+  p params
+  p "*" * 1000
+  @song = Song.new(params[:song]) #create new song
+  if @song.save #saves new song or returns false if unsuccessful
+    redirect '/songs' #redirect back to songs index page
+  else
+    redirect '/songs' # show new songs view again(potentially displaying errors)
+  end
+
 end
