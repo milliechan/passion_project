efbfa8be5f58d2054085b19aac5802c5dcb5fbcd
Millie Chan <mireechan@gmail.com>
2016-08-14 17:32:28 -0700

    Collect results into @songs hash and display on search/index.erb

--- app/controllers/search.rb
+++ app/controllers/search.rb
@@ -15,7 +15,10 @@ get '/search' do
   # GET https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=beyonce+karaoke&fields=items&key={YOUR_API_KEY}
 
   response = HTTParty.get(path)
-  p response.to_json 
+  # p response = response.to_json
+  p  "*" * 80 
+  # p response["items"]
+  # response = response.to_json
   # p response
   # GET https://www.googleapis.com/youtube/v3/search?q=radiohead&key={YOUR_API_KEY}
   # GET https://www.googleapis.com/youtube/v3/search
@@ -23,13 +26,21 @@ get '/search' do
 
   # youtube_link = "https://www.youtube.com/watch?v=" + videoId]
 
-  # items = response.items
-  # videoId = items[1].id.videoId 
-
-  # video_title = items[1].snippet.title
+  # videoId = response.items[1].id.videoId 
+  # videoTitle = response.items[1].snippet.title
+  # videoPhoto = response.items[1].snippet.thumbnails.default.url
   # thumbnail = items[1].snippet.thumbnails.default.url
-
-# "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&fields=items&key=#{ENV['YOUTUBE_KEY']}"
+  @songs = {} 
+  response["items"].each_with_index do |item, index|
+    @songs[index] = [ 
+      item["snippet"]["title"],
+      item["id"]["videoId"],
+      item["snippet"]["thumbnails"]["default"]["url"]
+    ]
+  end
+  
+  # if request.xhr? send partial with locals {songs = songs }
+   erb :'search/index'
 end
 
 post '/search' do 
